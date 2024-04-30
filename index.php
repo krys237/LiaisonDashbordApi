bbdc7012a6022ebdf78d2e121660004a52d56203
krys237 <168082315+krys237@users.noreply.github.com>
2024-04-30 09:05:18 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Index</title>
+</head>
+<body>
+    <h1 class="text-logo"><span class="">ENTERPRISES</span></h1>
+    <div class="container admin">
+        <div class="row">
+            <h1><strong>Liste des Services</strong><a href="" class="btn btn-success btn-lg"><span class="glyphicon glyphicon-plus">Ajouter</a>
+            <table classe="table table-stripped table-bordered"></table>
+            </h1>
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Description</th>
+                    <th>Prix</th>
+                    <th>Categorie</th>
+                    <th>Actions</th>
+
+                </tr>
+            <thead>
+                <td>Item 1</td>
+                <td>Descfription 1</td>
+                <td>Prix 1</td>
+                <td>Categorie 1</td>
+                <td width=300>
+                    <a class="btn btn-default" href=""><span class="glyphicon glyphicon-eye-open">Voir</a>
+                </td> 
+            </thead>
+            </thead>
+        </table>
+
+    </div>
+    
+</body>
+</html>
